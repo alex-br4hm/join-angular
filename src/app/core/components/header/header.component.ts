7694c0410f7e1d56de4a33af3cc09e41bcd4bcc8
br4hm <haehnlein.alexander@gmx.de>
2025-02-04 22:11:21 +0100

    JOI-21 JOI-22 JOI-24 implement header | navigation

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,14 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatButton} from "@angular/material/button";
+import {MatIcon} from '@angular/material/icon';
+import {NgClass} from '@angular/common';
+import {MatTooltip} from '@angular/material/tooltip';
+import {ActivatedRoute, NavigationEnd, Router, RouterLink} from '@angular/router';
+import {filter} from 'rxjs';
+import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
-    imports: [
-        MatButton
-    ],
+  imports: [
+    MatButton,
+    MatIcon,
+    NgClass,
+    MatTooltip,
+    RouterLink,
+    MatMenu,
+    MatMenuTrigger,
+    MatMenuItem
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  mode: string = 'light';
+  route: string = '';
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.route = event.urlAfterRedirects;
+      });
+    this.switchMode();
+  }
+
+  switchMode() {
+    this.mode = this.mode === 'light' ? 'dark' : 'light';
+
+    if (this.mode === 'light') {
+      document.body.classList.remove('dark-mode');
+    }
+
+    if (this.mode === 'dark') {
+      document.body.classList.add('dark-mode');
+    }
+  }
 }
