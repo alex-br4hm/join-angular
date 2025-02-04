4f6b9fcbeef2a8bf2eb125a519c90b2a10c77223
br4hm <haehnlein.alexander@gmx.de>
2025-02-04 22:14:00 +0100

    JOI-5 JOI-2 small style fix | clean-up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,13 @@
-import {Component, effect, OnInit, signal} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatButton} from '@angular/material/button';
 import {FirebaseService} from './core/services/firebase.service';
-import {ref} from '@angular/fire/storage';
-import {objectVal, onValue} from '@angular/fire/database';
-import {Observable} from 'rxjs';
 import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
 import {SideNavComponent} from './core/components/side-nav/side-nav.component';
 import {HeaderComponent} from './core/components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButton, AngularFirestoreModule, SideNavComponent, HeaderComponent],
+  imports: [RouterOutlet, AngularFirestoreModule, SideNavComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -20,13 +16,7 @@ export class AppComponent implements OnInit{
 
   constructor(private db: FirebaseService) {}
 
- applyDarkMode() {
-   document.body.classList.add('dark-mode');
- }
 
- applyLightMode() {
-   document.body.classList.remove('dark-mode');
- }
 
    ngOnInit() {
      this.db.getUsers().pipe(
