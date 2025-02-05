10fb8937592dcc4addd68dc482aaf20d06033db4
br4hm <haehnlein.alexander@gmx.de>
2025-02-05 01:24:59 +0100

    JOI-12 JOI-25 JOI-26 implement summary | display some correct data | loading-state

--- src/app/core/models/tasks.ts
+++ src/app/core/models/tasks.ts
@@ -1,2 +1,21 @@
-export interface Tasks {
+export interface Task {
+  assigned_user: AssignedUser[];
+  category: string;
+  description: string;
+  due_date: string;
+  id: number;
+  name: string;
+  priority: string;
+  state: string;
+  subtasks: Subtask[];
+}
+
+export interface Subtask {
+  isDone: boolean;
+  name: string;
+}
+
+export interface AssignedUser {
+  name: string;
+  first_two_letters: string;
 }
--- src/app/modules/summary/summary.component.ts
+++ src/app/modules/summary/summary.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FirebaseService} from '../../core/services/firebase.service';
+import { Task } from '../../core/models/tasks';
+import {MatIcon} from '@angular/material/icon';
+import {RouterLink} from '@angular/router';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-summary',
-  imports: [],
+  imports: [
+    MatIcon,
+    RouterLink,
+    MatProgressSpinner
+  ],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss'
 })
-export class SummaryComponent {
+export class SummaryComponent implements OnInit {
+  taskList!: Task[];
+  todoCount: number     = 0;
+  doneCount: number     = 0;
+  awaitCount: number    = 0;
+  progressCount: number = 0;
+  urgentCount: number   = 0;
+  tasksCount: number    = 0;
+  actualUser: string    = 'Alex Haehnlein';
+  isLoading: boolean    = true;
 
+  constructor(private firebase: FirebaseService) {
+  }
+
+  ngOnInit() {
+    this.firebase.getTasks().pipe(
+    ).subscribe({
+      next: data => {
+        this.taskList = data;
+        this.countValues();
+      },
+      error: error => {
+        console.log(error);
+      }
+    })
+  }
+
+  countValues() {
+    Object.values(this.taskList).forEach(task => {
+
+      if (task.state === 'todo')          this.todoCount++;
+      if (task.state === 'inprogress')    this.progressCount++;
+      if (task.state === 'awaitfeedback') this.awaitCount++;
+      if (task.state === 'done')          this.doneCount++;
+      if (task.state)                     this.tasksCount++;
+      if (task.priority === 'high')       this.urgentCount++;
+
+      if (this.tasksCount === Object.values(this.taskList).length) {
+        this.isLoading = false;
+      }
+    });
+  }
 }
