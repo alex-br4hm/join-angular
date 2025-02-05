c8c7948ce0c4daedbe31103da4b9a29ea77310b6
br4hm <haehnlein.alexander@gmx.de>
2025-02-05 10:48:41 +0100

    JOI-13 JOI-27 implement contact-list

--- src/app/modules/contacts/contacts.component.ts
+++ src/app/modules/contacts/contacts.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FirebaseService} from '../../core/services/firebase.service';
+import {Contact} from '../../core/models/contacts';
+import {MatButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-contacts',
-  imports: [],
+  imports: [
+    MatButton,
+    MatIcon
+  ],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
-export class ContactsComponent {
+export class ContactsComponent implements OnInit {
+  contactList!: Contact[];
+  sortedList: any[] = [
+    {
+      surname: "John",
+      lastName: "Doe",
+    },
+    {
+      surname: "Henry",
+      lastName: "Doe",
+    },
+    {
+      surname: "Alames",
+      lastName: "Doe",
+    }
+  ];
 
+  constructor(private firebase: FirebaseService) {}
+
+  ngOnInit() {
+    this.firebase.getContacts().pipe(
+    ).subscribe({
+      next: data => {
+        this.contactList = data;
+        console.log(this.contactList);
+      },
+      error: error => {
+        console.log(error);
+      }
+    })
+  }
 }
