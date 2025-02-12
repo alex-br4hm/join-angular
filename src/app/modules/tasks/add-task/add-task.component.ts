0162332c4bbc641f7f6a24eccbfe77a301c34068
br4hm <haehnlein.alexander@gmx.de>
2025-02-12 13:37:01 +0100

    JOI-14 JOI-30 implement add-task form

--- src/app/modules/tasks/add-task/add-task.component.ts
+++ src/app/modules/tasks/add-task/add-task.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatError, MatFormField, MatLabel} from '@angular/material/form-field';
 import {MatInput, MatInputModule} from '@angular/material/input';
-import {MatOption, MatSelect} from '@angular/material/select';
+import {MatOption, MatSelect, MatSelectTrigger} from '@angular/material/select';
 import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from '@angular/material/datepicker';
 import {MatNativeDateModule} from '@angular/material/core';
 import {MatRadioButton, MatRadioGroup} from '@angular/material/radio';
 import {MatButton, MatIconButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
+import {FirebaseService} from '../../../core/services/firebase.service';
+import {Contact} from '../../../core/models/contacts';
+import {NgStyle} from '@angular/common';
+import {MatTooltip} from '@angular/material/tooltip';
+import {MatButtonToggle, MatButtonToggleGroup} from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-add-task',
@@ -23,26 +28,69 @@ import {MatIcon} from '@angular/material/icon';
     MatDatepickerInput,
     MatNativeDateModule,
     MatInputModule,
-    MatRadioGroup,
-    MatRadioButton,
     MatButton,
-    MatIcon
+    MatIcon,
+    MatSelectTrigger,
+    NgStyle,
+    MatTooltip,
+    MatButtonToggleGroup,
+    MatButtonToggle
   ],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss'
 })
-export class AddTaskComponent {
+export class AddTaskComponent implements OnInit {
   addTaskForm: FormGroup;
+  assignableUser: Contact[] = [];
+  assignedUser: Contact[] = []
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private fireBase: FirebaseService,) {
     this.addTaskForm = this.fb.group({
       title: ['', Validators.required],
       due_date: ['', Validators.required],
       category: ['', Validators.required],
       description: '',
       assigned_to: '',
-      priority: ''
+      priority: 'medium'
     });
+  }
+
+  ngOnInit() {
+    this.fireBase.getUsers().pipe(
+    ).subscribe({
+      next: data => {
+        this.getAssignableUser(data);
+      },
+      error: error => {
+        console.log(error);
+      }
+    })
+
+    this.addTaskForm.get('assigned_to')?.valueChanges.subscribe((assignedValue) => {
+      this.assignedUser = assignedValue.map((id: number)  => {
+        return this.assignableUser.find(user => user.id === id);
+      })
+    });
+  }
+
+  getAssignableUser(users: any) {
+    this.assignableUser = Object.values(users).map((user: any) => ({
+      id: user.id,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      color: user.color,
+      phone: user.phone,
+      email: user.email,
+    }));
+  }
+
+
+  removeUserFromAssignedList(id: number) {
+    this.addTaskForm.get('assigned_to')?.setValue(
+      this.addTaskForm.get('assigned_to')?.value.filter((userId: number) => userId !== id)
+    );
   }
 
   onSubmit() {
@@ -52,4 +100,5 @@ export class AddTaskComponent {
       console.log('Formular ungültig');
     }
   }
+
 }
