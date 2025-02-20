b3946ab3569cbce4522a6b0116ba65ecbb3e6dc2
br4hm <haehnlein.alexander@gmx.de>
2025-02-20 22:45:49 +0100

    JOI-5 implement first-letter-pipe

---
+++ src/app/shared/utils/first-letter.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'firstLetter'
+})
+export class FirstLetterPipe implements PipeTransform {
+
+  transform(name: string): string {
+    return name.toUpperCase().charAt(0);
+  }
+
+}
