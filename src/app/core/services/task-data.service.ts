155c72fe4ba18b17d701819572b7518702dd69cb
br4hm <haehnlein.alexander@gmx.de>
2025-03-08 15:01:02 +0100

    JOI-14 implement addTask function

--- src/app/core/services/task-data.service.ts
+++ src/app/core/services/task-data.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {Database, push, ref, set} from '@angular/fire/database';
+import {Task} from '../models/tasks';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskDataService {
+  private db = inject(Database);
 
   constructor() { }
+
+  addTask(task: Task, state: string) {
+    console.log(task);
+    const tasksRef    = ref(this.db, 'tasks');
+    const newTaskRef  = push(tasksRef);
+
+    if (newTaskRef.key != null) {
+      task.id = newTaskRef.key;
+    }
+
+    task.state = state;
+
+    set(newTaskRef, task)
+      .then(() => {
+        console.log('Task added successfully');
+      })
+      .catch((error) => {
+        console.error('Error adding contact: ', error);
+      });
+  }
 }
