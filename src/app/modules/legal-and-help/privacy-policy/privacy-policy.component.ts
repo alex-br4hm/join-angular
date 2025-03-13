3af5d1b14342dac01503269c0cf1423b836faa7f
br4hm <haehnlein.alexander@gmx.de>
2025-03-13 08:18:32 +0100

    JOI-53 implement help | legal notice | privacy policy

--- src/app/modules/legal-and-help/privacy-policy/privacy-policy.component.ts
+++ src/app/modules/legal-and-help/privacy-policy/privacy-policy.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [],
+  imports: [
+    MatIcon
+  ],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
