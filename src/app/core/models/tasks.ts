3974cf49ec7c3d00adbb8e60b179bed3c41f9d43
br4hm <haehnlein.alexander@gmx.de>
2025-03-13 20:29:04 +0100

    JOI-14 implement taskCategories interface

--- src/app/core/models/tasks.ts
+++ src/app/core/models/tasks.ts
@@ -5,12 +5,16 @@ export interface Task {
   due_date:      string;
   due_date_unix: number;
   id:            string;
-  // surname:       string;
-  // lastname:      string;
   title:         string;
   priority:      string;
   state:         string;
   subtasks?:     Subtask[];
+}
+
+export interface TaskCategories {
+  name: string;
+  state: string;
+  list: Task[];
 }
 
 export interface Subtask {
