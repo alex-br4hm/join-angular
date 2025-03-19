0a909b9896355a32786cb0ba5945e708a1c76840
br4hm <haehnlein.alexander@gmx.de>
2025-03-19 08:06:42 +0100

    JOI-14 adjustments task-card in board

--- src/app/modules/board/task-card/task-card.component.ts
+++ src/app/modules/board/task-card/task-card.component.ts
@@ -4,6 +4,9 @@ import {SubtaskProgressPipe} from '../../../shared/utils/subtask-progress.pipe';
 import {MatTooltip} from '@angular/material/tooltip';
 import {MatIcon} from '@angular/material/icon';
 import {PrioIconPipe} from '../../../shared/utils/prio-icon.pipe';
+import {FirstLetterPipe} from '../../../shared/utils/first-letter.pipe';
+import {SliceAssignedUserPipe} from '../../../shared/utils/slice-assigned-user.pipe';
+import {AssignedUserOverflowPipe} from '../../../shared/utils/assigned-user-overflow.pipe';
 
 
 @Component({
@@ -12,7 +15,10 @@ import {PrioIconPipe} from '../../../shared/utils/prio-icon.pipe';
     SubtaskProgressPipe,
     MatTooltip,
     MatIcon,
-    PrioIconPipe
+    PrioIconPipe,
+    FirstLetterPipe,
+    SliceAssignedUserPipe,
+    AssignedUserOverflowPipe
   ],
   templateUrl: './task-card.component.html',
   styleUrl: './task-card.component.scss'
