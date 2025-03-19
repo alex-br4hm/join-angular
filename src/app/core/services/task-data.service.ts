753b1a89b973508820dc1709190d149fdad204d7
br4hm <haehnlein.alexander@gmx.de>
2025-03-19 08:05:48 +0100

    JOI-14 implement delete / patch

--- src/app/core/services/task-data.service.ts
+++ src/app/core/services/task-data.service.ts
@@ -1,5 +1,5 @@
 import {inject, Injectable} from '@angular/core';
-import {Database, push, ref, set} from '@angular/fire/database';
+import {Database, push, ref, remove, set, update} from '@angular/fire/database';
 import {Task} from '../models/tasks';
 
 @Injectable({
@@ -8,27 +8,53 @@ import {Task} from '../models/tasks';
 export class TaskDataService {
   private db         = inject(Database);
   taskState: string             = 'todo';
-  tasksRef   = ref(this.db, 'tasks');
-  newTaskRef = push(this.tasksRef);
+
 
 
   constructor() { }
 
   addTask(task: Task) {
+    const tasksRef   = ref(this.db, 'tasks');
+    const newTaskRef = push(tasksRef);
     console.log(task);
     task.state = this.taskState;
     task.due_date_unix = this.getUnixTimeStamp(task.due_date);
 
-    if (this.newTaskRef.key != null) {
-      task.id = this.newTaskRef.key;
+    if (newTaskRef.key != null) {
+      task.id = newTaskRef.key;
     }
 
-    set(this.newTaskRef, task)
+    set(newTaskRef, task)
       .then(() => {
         console.log('Task added successfully');
       })
       .catch((error) => {
         console.error('Error adding contact: ', error);
+      });
+  }
+
+  deleteTask(taskID: string) {
+    const taskRef = ref(this.db, `tasks/${taskID}`);
+    console.log(taskID);
+
+    remove(taskRef)
+      .then(() => {
+        console.log('Task deleted', taskRef);
+      })
+      .catch((error) => {
+        console.error('Error deleting contact: ', error);
+      });
+  }
+
+  patchTask(task: Task) {
+    const taskRef = ref(this.db, `tasks/${task.id}`);
+
+    update(taskRef, task)
+      .then(() => {
+        console.log('Task successfully updated', task);
+      })
+      .catch((error) => {
+        console.error('Error updating task:', error);
       });
   }
 
