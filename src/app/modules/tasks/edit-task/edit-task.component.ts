890e7caffc7ed56d6096f3ddcb194cc7192a4ab4
br4hm <haehnlein.alexander@gmx.de>
2025-03-19 08:09:05 +0100

    JOI-14 implement edit-task view

--- src/app/modules/tasks/edit-task/edit-task.component.ts
+++ src/app/modules/tasks/edit-task/edit-task.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, inject, Input} from '@angular/core';
+import { Task } from '../../../core/models/tasks';
+import {TaskDataService} from '../../../core/services/task-data.service';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatError, MatFormField, MatLabel, MatSuffix} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-edit-task',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatError,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatDatepicker,
+    MatDatepickerInput,
+    MatDatepickerToggle,
+    MatSuffix
+  ],
   templateUrl: './edit-task.component.html',
   styleUrl: './edit-task.component.scss'
 })
 export class EditTaskComponent {
+  @Input() task!: Task;
+  fb: FormBuilder    = inject(FormBuilder);
+  editTaskForm!: FormGroup;
+  today: Date = new Date();
 
+  constructor (private taskDataService: TaskDataService) {
+  }
+
+  ngOnInit() {
+    console.log(this.task);
+    this.editTaskForm = this.fb.group({
+      title: [this.task.title, Validators.required],
+      due_date: ['12-10-2025', Validators.required],
+    })
+  }
 }
