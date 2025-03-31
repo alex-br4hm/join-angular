2276ec649fbe83c5224e4061ec6f6a90823e6865
br4hm <haehnlein.alexander@gmx.de>
2025-03-31 12:09:06 +0200

    JOI-5 adjustments

--- src/app/core/services/date-formatter.service.ts
+++ src/app/core/services/date-formatter.service.ts
@@ -16,4 +16,20 @@ export class DateFormatterService {
 
     return formatter.format(date).replaceAll('.', '/');
   }
+
+  formatToDate(date: string | undefined): Date {
+    let dateParts: string[] | number[] | undefined = date?.split('/');
+    dateParts = dateParts?.map((part) => Number(part));
+
+    if (dateParts) {
+      return new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+    }
+
+    return new Date();
+  }
+
+  unixToDate(unix: number | undefined): Date {
+    if (!unix) return new Date();
+    return new Date(unix * 1000);
+  }
 }
