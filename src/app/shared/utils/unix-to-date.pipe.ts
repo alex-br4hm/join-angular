943e3b16f97f1ebe8c3eada8a02892bb4a1f8e79
br4hm <haehnlein.alexander@gmx.de>
2025-03-31 12:08:47 +0200

    JOI-5 unix to date pipe

---
+++ src/app/shared/utils/unix-to-date.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'unixToDate'
+})
+export class UnixToDatePipe implements PipeTransform {
+
+  transform(unix: number): string {
+    console.log(unix);
+    if (!unix) return '';
+
+    const date = new Date(unix * 1000);
+    console.log(date);
+    if (isNaN(date.getTime())) return '';
+
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+  }
+
+}
