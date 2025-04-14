ccd9f9478dfb55451fac738d6b804eec497f50c4
br4hm <haehnlein.alexander@gmx.de>
2025-04-14 13:09:28 +0200

    JOI-5 refactor delete dialog

--- src/app/shared/ui/delete-dialog/delete-dialog.component.ts
+++ src/app/shared/ui/delete-dialog/delete-dialog.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import {MatDialogActions, MatDialogContent, MatDialogRef, MatDialogTitle} from '@angular/material/dialog';
 import {MatButton} from '@angular/material/button';
+import {MatIcon} from "@angular/material/icon";
+import {MatSuffix} from "@angular/material/form-field";
 
 @Component({
   selector: 'app-delete-dialog',
-  imports: [
-    MatDialogContent,
-    MatDialogTitle,
-    MatDialogActions,
-    MatButton
-  ],
+    imports: [
+        MatDialogContent,
+        MatDialogTitle,
+        MatDialogActions,
+        MatButton,
+        MatIcon,
+        MatSuffix
+    ],
   templateUrl: './delete-dialog.component.html',
   styleUrl: './delete-dialog.component.scss'
 })
